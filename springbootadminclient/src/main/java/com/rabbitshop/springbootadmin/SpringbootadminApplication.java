939c0b04b3e27c1df9898f4b8ed287fa28117e3c
Matteo Baiguini <mab@foryouandyourcustomers.com>
2018-05-23 17:18:21 +0200

    Spring Boot Admin sample first look

---
+++ springbootadminclient/src/main/java/com/rabbitshop/springbootadmin/SpringbootadminApplication.java
@@ -0,0 +1,12 @@
+package com.rabbitshop.springbootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootadminApplication.class, args);
+	}
+}
