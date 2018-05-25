7d4bde97756cc183c6d3bb19793f6bf9e46e49a4
Matteo Baiguini <mab@foryouandyourcustomers.com>
2018-05-25 14:16:52 +0200

    SpringBootAdmin EurekaClient sample first draft (not working)

---
+++ springbootadmineurekaclient/src/main/java/com/rabbitshop/springbootadmineurekaclient/SpringBootAdminEurekaClientApplication.java
@@ -0,0 +1,20 @@
+package com.rabbitshop.springbootadmineurekaclient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringBootAdminEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminEurekaClientApplication.class, args);
+	}
+}
