bf4b1ad308f04c1396a764ae782306818b6ed8d9
Matteo Baiguini <mab@foryouandyourcustomers.com>
2018-05-25 14:16:40 +0200

    SpringBootAdmin ConsulClient sample first draft (not working)

---
+++ springbootadminconsulclient/src/main/java/com/rabbitshop/springbootadminconsulclient/SpringBootAdminConsulClientApplication.java
@@ -0,0 +1,12 @@
+package com.rabbitshop.springbootadminconsulclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminConsulClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminConsulClientApplication.class, args);
+	}
+}
