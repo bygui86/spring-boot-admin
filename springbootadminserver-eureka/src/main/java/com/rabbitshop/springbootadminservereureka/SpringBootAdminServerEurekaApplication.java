5dfcffd72e085f68e07d903c6ef21ff79b0f6bbf
Matteo Baiguini <baiguini.matteo@gmail.com>
2018-06-11 00:46:22 +0200

    Small updates

--- springbootadminserver-eureka/src/main/java/com/rabbitshop/springbootadminservereureka/SpringBootAdminServerEurekaApplication.java
+++ springbootadminserver-eureka/src/main/java/com/rabbitshop/springbootadminservereureka/SpringBootAdminServerEurekaApplication.java
@@ -3,13 +3,13 @@ package com.rabbitshop.springbootadminservereureka;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
-@EnableDiscoveryClient
+@SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
+@EnableAutoConfiguration
 public class SpringBootAdminServerEurekaApplication {
 
 	public static void main(String[] args) {
--- springbootadminserver-eureka/src/main/java/com/rabbitshop/springbootadminservereureka/configs/SecurityConfig.java
+++ springbootadminserver-eureka/src/main/java/com/rabbitshop/springbootadminservereureka/configs/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.rabbitshop.springbootadminservereureka.configs;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Slf4j
 @Configuration
+@EnableAutoConfiguration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	/*
@@ -12,6 +16,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
+
+		log.debug("Loading SECURITY config...");
 
 		http
 				.authorizeRequests()
